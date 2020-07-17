ebd8975a1d19124d460d3dd537467a4e9db9a67c
adminuser <adminuser@adminusers-MBP.attlocal.net>
2020-07-17 19:06:13 -0400

    created large for loop

--- import_data_V2.r
+++ import_data_V2.r
@@ -26,23 +26,28 @@ datacompleteness = c()
 # MaxMOI for each locus 
 for (j in 1:nloci) {
 	locicolumns = grepl(paste(locinames[j],"",sep=""),colnames(data))
-	print(colnames(data))
-	print(locicolumns)
+	#print(colnames(data))
+	#print(locicolumns)
 	raw_alleles = data[,locicolumns]
+	print(raw_alleles)
+	#print(rowSums(raw_alleles == "X",na.rm=TRUE))
 	maxMOI = max(c(2,rowSums(raw_alleles == "X",na.rm=TRUE)))
+	#print(maxMOI)
 	MOI = rowSums(raw_alleles == "X",na.rm=TRUE)
 	nalleles = sum(locicolumns,na.rm=TRUE)
+	#print(nalleles)
 	newdatatemp = rbind(matrix(NA,nids,maxMOI))
 	sapply(1:nids,function (x) if (MOI[x] > 0) { newdatatemp[x,1:MOI[x]] <<- paste("Hap_",which(raw_alleles[x,] == "X"),sep="")})
 	if (ploidy[j] > 1) {
 		sapply(1:nids,function (x) if (MOI[x] == 1) { newdatatemp[x,1:2] <<- paste("Hap_",which(raw_alleles[x,] == "X"),sep="")})
 	}
 	colnames(newdatatemp) = paste(1:maxMOI,"_Hap_",locinames[j],sep="")
+	#print(newdatatemp)
 	newdata = cbind(newdata,newdatatemp )
 	datacompleteness  = cbind(datacompleteness,MOI)
 }
 
-print(ncol(newdata))
+#print(ncol(newdata))
 data = cbind(ids,newdata)
 data = data.frame(data)
 
@@ -53,8 +58,8 @@ datacompleteness_bylocus = sapply(locinames_base,function (x) rowSums(cbind(data
 print("Number of loci sequenced")
 table(rowSums(datacompleteness_bylocus))
 
-print(datacompleteness_bylocus)
-print(rowSums(datacompleteness_bylocus))
+#print(datacompleteness_bylocus)
+#print(rowSums(datacompleteness_bylocus))
 
 
 # define inclusion criteria below - filters genotype datasheet to exclude specimens that fail to meet these criteria
@@ -70,16 +75,16 @@ cleandata = data[(rowSums(datacompleteness_bylocus[,c(5,7,8)]) == 3 & rowSums(da
 #print(cleandata)
 print((rowSums(datacompleteness_bylocus[,c(5,7,8)]) == 3 & rowSums(datacompleteness_bylocus) >= 4) | rowSums(datacompleteness_bylocus) >= 5 | (rowSums(datacompleteness_bylocus[,c(6,7,8)]) == 3 & rowSums(datacompleteness_bylocus) >= 4) | (rowSums(datacompleteness_bylocus[,c(5,6,7)]) == 3 & rowSums(datacompleteness_bylocus) >= 4) | (rowSums(datacompleteness_bylocus[,c(5,6,8)]) == 3 & rowSums(datacompleteness_bylocus) >= 4))
 
-write.csv(cleandata,"/Users/adminuser/Desktop/CDC/Bayesian/pyamd/cleandata.csv", row.names = FALSE )
+#write.csv(cleandata,"/Users/adminuser/Desktop/CDC/Bayesian/pyamd/cleandata.csv", row.names = FALSE )
 nrow(cleandata)
 nrow(data)
 ncol(cleandata)
 
 data = cleandata
 ids = data$ids
-print(ids)
+#print(ids)
 nids = length(ids)
 
-print(nids)
+#print(nids)
 
 ####write.csv(data,"transformed_data.csv")
