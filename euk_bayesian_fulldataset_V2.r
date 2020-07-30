c9091b2970d5b60951f39c9e78c47907c302908c
adminuser <adminuser@adminusers-MBP.attlocal.net>
2020-07-30 13:08:27 -0400

    likelihood1fix2

--- euk_bayesian_fulldataset_V2.r
+++ euk_bayesian_fulldataset_V2.r
@@ -58,12 +58,19 @@ calculate_loglikelihood2 = function(v1,v2,p1,p2,ploid){
 	pairs2 = combn(v2,2,simplify = FALSE)
 	npairs1 = length(pairs1)
 	npairs2 = length(pairs2)
-
+  #print(p1)
+  #print(p2)
 	loglikelihood2 = log(max(sapply(1:npairs1, function (i) sapply(1:npairs2, 
 				function (j) (sum(sort(pairs1[[i]]) == sort(pairs2[[j]]))==2)*
 							exp((sum(log(p1[-match(pairs1[[i]],v1)]))+sum(log(p2)))))),na.rm=TRUE))
 	} else { loglikelihood2 = NA}
   #print(loglikelihood1)
+	print(p1)
+	print(v1)
+	print(pairs1)
+	print(-match(pairs1[[1]],v1))
+	print(exp(sum(log(p1[-match(pairs1[[1]],v1)]))+sum(log(p2))))
+	#print(p1[-match(pairs1[[2]],v1)])
 	if (loglikelihood1 == -Inf) {
 		loglikelihood1 = log(epsilon * min(c(p1,p2)))
 	}
