fd0ab2f3f436403866a936b4ac58644b48e9796a
adminuser <adminuser@adminusers-MBP.attlocal.net>
2020-08-10 15:38:35 -0400

    likelihood2fixing4

--- pyamd/euk_bayesian_fulldataset_V2.r
+++ pyamd/euk_bayesian_fulldataset_V2.r
@@ -54,9 +54,11 @@ calculate_loglikelihood2 = function(v1,v2,p1,p2,ploid){
 				function (j) (sum(sort(pairs1[[i]]) == sort(pairs2[[j]]))==2)*
 							exp((sum(log(p1[-match(pairs1[[i]],v1)]))+sum(log(p2)))))),na.rm=TRUE))
 	} else { loglikelihood2 = NA}
-	print(p1)
-	print(-match(pairs1[[1]],v1))
-  print(p1[-match(pairs1[[1]],v1)])
+	#print(p1)
+	#print(-match(pairs1[[1]],v1))
+  #print(p1[-match(pairs1[[1]],v1)])
+  #print(exp((sum(log(p1[-match(pairs1[[1]],v1)]))+sum(log(p2)))))
+  #print(exp(sum(log(p2))))
 	if (loglikelihood1 == -Inf) {
 		loglikelihood1 = log(epsilon * min(c(p1,p2)))
 	}
